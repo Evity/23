7e7f4d4ff7a730eace2206812cc1ac9744be87ef
Evity <1172432785@qq.com>
2022-09-22 00:58:03 +0800

    update adapter

--- Patterns/StructralPatterns/Adapter/adapter.go
+++ Patterns/StructralPatterns/Adapter/adapter.go
@@ -1,1 +1,52 @@
 package main
+
+import "fmt"
+
+// Object to adapt
+type BigBrother interface {
+	Watch()
+}
+
+// tx class, depend on bigBrother
+type Governor struct {
+	boss BigBrother
+}
+
+func NewGovernor(b BigBrother) *Governor {
+	return &Governor{b}
+}
+
+func (g *Governor) Govern() {
+	fmt.Println("Governing")
+	g.boss.Watch()
+}
+
+// object be adapted
+type People struct{}
+
+func (p *People) Live() {
+	fmt.Println("People surviving")
+}
+
+// adapter
+type Monitor struct {
+	p *People
+}
+
+func (m *Monitor) Watch() {
+	fmt.Println("Big brother is watching you!")
+
+	//call adapter's method
+	m.p.Live()
+}
+
+func NewMonitor(p *People) *Monitor {
+	return &Monitor{p}
+}
+
+// tx logic layer
+func main() {
+	party := NewGovernor(NewMonitor(new(People)))
+
+	party.Govern()
+}
