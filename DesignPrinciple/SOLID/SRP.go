c92fa3ed3b975c16eb1077034f56a12d16a348cf
Evity <1172432785@qq.com>
2022-10-03 16:58:29 +0800

    modify SRP

--- DesignPrinciple/SOLID/SRP.go
+++ DesignPrinciple/SOLID/SRP.go
@@ -2,26 +2,50 @@ package main
 
 import "fmt"
 
-type Cat struct{}
+//A class should have one, and only one, reason to change.
+//Single responsibility principle
 
-func (c *Cat) Roar() {
-	fmt.Println("Meow~")
+/* type Clothes struct{}
+
+func (c *Clothes) OnWork() {
+	fmt.Println("Working Uniform")
 }
 
-type Carrot struct{}
-
-func (c *Carrot) Imitate() {
-	fmt.Println("Hello!")
+func (c *Clothes) OnShop() {
+	fmt.Println("Shopping Clothes")
 }
 
 func main() {
-	//carrot polymorph
-	carrot := new(Carrot)
-	carrot.Imitate()
+	c := Clothes{}
 
-	//cat polymorph
-	cat := new(Cat)
-	cat.Roar()
+	// Working
+	fmt.Println("Working...")
+	c.OnWork()
+
+	// Shopping
+	fmt.Println("Shopping...")
+	c.OnShop()
+}
+*/
+
+type ClothesShop struct{}
+
+func (cs *ClothesShop) Style() {
+	fmt.Println("On shopping")
 }
 
-//A class should have one, and only one, reason to change.
+type ClothesWork struct{}
+
+func (cw *ClothesWork) Style() {
+	fmt.Println("On working")
+}
+
+func main() {
+	//Working tx
+	cw := ClothesWork{}
+	cw.Style()
+
+	//Shopping tx
+	cs := ClothesShop{}
+	cs.Style()
+}
